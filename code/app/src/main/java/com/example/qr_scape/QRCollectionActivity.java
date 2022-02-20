8b8f78dec4f7ceb0b6358ad712e51d21d01ce14e
Ty Greve <tgreve@ualberta.ca>
2022-02-19 21:39:33 -0700

    deleteQRCode with database connection.

--- code/app/src/main/java/com/example/qr_scape/QRCollectionActivity.java
+++ code/app/src/main/java/com/example/qr_scape/QRCollectionActivity.java
@@ -7,5 +7,23 @@ public class QRCollectionActivity {
 
 
 
-    
+    public void deleteQRCodeClicked(View view) {
+        if (currentlySelected == -1){
+            Toast.makeText(QRCollectionActivity.this, "No QR Code Selected", Toast.LENGTH_SHORT).show();
+        } else {
+            String QRID = QRCollectionDataList.get(currentlySelected).getQRID().toString();
+            if (QRID.equals(null)){
+                Toast.makeText(QRCollectionActivity.this, "No QR Code Selected", Toast.LENGTH_SHORT).show();
+            } else {
+                // From: https://www.youtube.com/
+                // URL: https://www.youtube.com/watch?v=1gerxvFAGio
+                // Author: https://www.youtube.com/channel/UC_Fh8kvtkVPkeihBs42jGcA
+                // Licence: https://creativecommons.org/licenses/by-sa/3.0/
+                DocumentReference QRRef = db.collection("QRCodes").document(QRID);
+                QRRef.delete();
+                QRCollectionAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
+            }
+        }
+        currentlySelected = -1; // Reset selected after deletion
+    }
 }
