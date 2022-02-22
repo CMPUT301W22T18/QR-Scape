75d81b2ebc3189a73e6ae17c430336d02cd4e104
Ty Greve <tgreve@ualberta.ca>
2022-02-22 16:09:25 -0700

    QRCode class created. calculateScore, generateQRID, generateImageQR not implemented.

--- code/app/src/main/java/com/example/qr_scape/QRCode.java
+++ code/app/src/main/java/com/example/qr_scape/QRCode.java
@@ -1,10 +1,38 @@
 package com.example.qr_scape;
 
+import java.io.File;
+
 public class QRCode {
     private String QRID;
     private String location;
+    private int score;
+    private File imageQR;
 
-
+    // Constructors
+    public QRCode(String QRID) { // Only QRID provided
+        this.QRID = QRID;
+        this.score = calculateScore(QRID);
+        this.imageQR = generateImageQR(QRID);
+        this.location = null;
+    }
+    public QRCode(String QRID, String location) { // QRID and location provided
+        this.QRID = QRID;
+        this.location = location;
+        this.score = calculateScore(QRID);
+        this.imageQR = generateImageQR(QRID);
+    }
+    public QRCode(File imageQR) { // Only imageQR provided
+        this.imageQR = imageQR;
+        this.QRID = generateQRID(imageQR);
+        this.score = calculateScore(QRID);
+        this.location = null;
+    }
+    public QRCode(String location, File imageQR) { // imageQR and location
+        this.location = location;
+        this.imageQR = imageQR;
+        this.QRID = generateQRID(imageQR);
+        this.score = calculateScore(QRID);
+    }
 
     public String getQRID() {
         return QRID;
@@ -13,4 +41,47 @@ public class QRCode {
     public void setQRID(String QRID) {
         this.QRID = QRID;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public File getImageQR() {
+        return imageQR;
+    }
+
+    public void setImageQR(File imageQR) {
+        this.imageQR = imageQR;
+    }
+
+    private int calculateScore(String QRID){
+        int score = -1;
+        // To do: Write the logic to calculate a QR codes score based on the QRID
+        return score;
+    }
+
+    private String generateQRID(File imageQR){
+        String newQRID = null;
+        // To do: Write the logic to generate a QRID based on the imageQR
+        return newQRID;
+    }
+
+    private File generateImageQR(String QRID){
+        File newImageQR = null;
+        // To do: Write the logic to generate a imageQR based on the QRID
+        return newImageQR;
+    }
+
 }
--- code/app/src/main/java/com/example/qr_scape/QRCollectionActivity.java
+++ code/app/src/main/java/com/example/qr_scape/QRCollectionActivity.java
@@ -11,30 +11,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
-public class QRCollectionActivity extends AppCompatActivity {
-    public int currentlySelected = -1;
-    FirebaseFirestore db;
-    ArrayAdapter<QRCode> QRCollectionAdapter;
-    ArrayList<QRCode> QRCollectionDataList;
+public class QRCollectionActivity {
 
-
-    public void deleteQRCodeClicked(View view) {
-        if (currentlySelected == -1){
-            Toast.makeText(QRCollectionActivity.this, "No QR Code Selected", Toast.LENGTH_SHORT).show();
-        } else {
-            String QRID = QRCollectionDataList.get(currentlySelected).getQRID().toString();
-            if (QRID.equals(null)){
-                Toast.makeText(QRCollectionActivity.this, "No QR Code Selected", Toast.LENGTH_SHORT).show();
-            } else {
-                // From: https://www.youtube.com/
-                // URL: https://www.youtube.com/watch?v=1gerxvFAGio
-                // Author: https://www.youtube.com/channel/UC_Fh8kvtkVPkeihBs42jGcA
-                // Licence: https://creativecommons.org/licenses/by-sa/3.0/
-                DocumentReference QRRef = db.collection("QRCodes").document(QRID);
-                QRRef.delete();
-                QRCollectionAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched from the cloud
-            }
-        }
-        currentlySelected = -1; // Reset selected after deletion
-    }
 }
