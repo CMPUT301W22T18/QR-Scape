9f0570fa1eb59fa4dc79464038d3c0f90b4781ba
Harsh G Shah <46387694+hgshah@users.noreply.github.com>
2022-02-26 02:32:44 -0700

    Layoutgradient

---
+++ main/java/com/example/qr_scape/QR_Scan.java
@@ -0,0 +1,14 @@
+package com.example.qr_scape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QR_Scan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+    }
+}
