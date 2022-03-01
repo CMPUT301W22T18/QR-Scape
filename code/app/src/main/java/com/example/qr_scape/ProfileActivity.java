2677fa6c71229357cbb923651d34a202534054f1
Dallin Dmytryk <dallindmytryk@gmail.com>
2022-02-28 21:30:41 -0700

    now supports pulling contact info from firestore, and writing new contact info to firestore

--- code/app/src/main/java/com/example/qr_scape/ProfileActivity.java
+++ code/app/src/main/java/com/example/qr_scape/ProfileActivity.java
@@ -11,12 +11,26 @@ package com.example.qr_scape;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
 
 /**
  * ProfileActivity
@@ -26,6 +40,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
  * @version 1
  */
 public class ProfileActivity extends AppCompatActivity {
+    final String USERNAME = "Username";
+    final String PROFILES = "Profiles";
+    final String CONTACTINFO = "Contact info";
     TextView usernameText;
     EditText contactInfoText;
     FirebaseFirestore db;
@@ -37,8 +54,69 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.background));
 
-        usernameText = findViewById(R.id.profile_username_text);
+        // Check shared preferences for username
+        sharedPreferences = getSharedPreferences(String.valueOf(R.string.app_name),MODE_PRIVATE);
+        String savedUserName = sharedPreferences.getString(USERNAME,null);
 
+        // Initialize database
+        db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection(PROFILES).document(savedUserName);
+
+        // Have field update
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (value.exists()) {
+                    Log.d(null,"Successfully loaded profile");
+                    String infoText = value.getString(CONTACTINFO);
+                    contactInfoText.setText(infoText);
+                } else {
+                    Log.d(null, "Couldn't load profile");
+                }
+            }
+        });
+
+        // Set username text
+        usernameText = findViewById(R.id.profile_username_text);
+        usernameText.setText(savedUserName);
+
+        // Set intractable contact info text
         contactInfoText = findViewById(R.id.profile_contact_info_edittext);
+        contactInfoText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    HashMap data = new HashMap<>();
+                    String infoText = contactInfoText.getText().toString();
+                    data.put(CONTACTINFO, infoText);
+                    documentReference.update(data)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(null, "Successfully updated contact info");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(null, "Failed to update contact info");
+                                }
+                            });
+                }
+            }
+        });
+        contactInfoText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                switch (i){
+                    case EditorInfo.IME_ACTION_DONE:
+                    case EditorInfo.IME_ACTION_NEXT:
+                    case EditorInfo.IME_ACTION_PREVIOUS:
+                        contactInfoText.clearFocus();
+                }
+                return true;
+            }
+        });
     }
+
 }
