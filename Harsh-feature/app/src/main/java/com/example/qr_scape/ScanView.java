82104318cd190ad75081f7b3726ed29b3af0e606
Harsh G Shah <46387694+hgshah@users.noreply.github.com>
2022-03-09 17:57:00 -0700

    Scan/Generate/Geo

---
+++ Harsh-feature/app/src/main/java/com/example/qr_scape/ScanView.java
@@ -0,0 +1,62 @@
+package com.example.qr_scape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanView extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    ZXingScannerView ScanView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ScanView=new ZXingScannerView(this);
+        setContentView(ScanView);
+        Dexter.withContext(getApplicationContext())
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        ScanView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+    @Override
+    public void handleResult(Result rawResult) {
+        QR_Scan.scantext.setText(rawResult.getText());
+        onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ScanView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ScanView.setResultHandler(this);
+        ScanView.startCamera();
+    }
+}
