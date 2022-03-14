377d242e56e65adb0b0839879e6c97cf91430353
Ty Greve <tgreve@ualberta.ca>
2022-03-14 14:45:58 -0600

    QR_ScanTest class added.

---
+++ app/src/test/java/com/example/qr_scape/QR_ScanTest.java
@@ -0,0 +1,13 @@
+package com.example.qr_scape;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.nio.charset.StandardCharsets;
+
+public class QR_ScanTest {
+
+    @Test
+    public void addQRCodeTest(){
+
+    }
+}
