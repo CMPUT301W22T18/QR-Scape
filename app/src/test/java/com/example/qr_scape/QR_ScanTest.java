a64927a364500c158bd07100e2f76e7efbb41185
Harsh Shah <hgshah@ualberta.ca>
2022-03-14 15:36:03 -0600

    update

--- app/src/test/java/com/example/qr_scape/QR_ScanTest.java
+++ app/src/test/java/com/example/qr_scape/QR_ScanTest.java
@@ -1,11 +1,24 @@
 package com.example.qr_scape;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.nio.charset.StandardCharsets;
 
 public class QR_ScanTest {
+    @Test
+    public void ScanQRCodeTest(){
+//        assertEquals(1, 1);
+    }
 
+    @Test
+    public void CaptureImageTest(){
+
+    }
+    @Test
+    public void GeoLocationTest(){
+
+    }
     @Test
     public void addQRCodeTest(){
 
