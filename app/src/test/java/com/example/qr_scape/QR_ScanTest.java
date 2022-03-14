f6a3f6f7f6f701567cd3587e9951d60f5caae3d0
Ty Greve <tgreve@ualberta.ca>
2022-03-14 15:08:56 -0600

    Started tests inside QR_ScanTest for addQRCode method, but they are unfinished.

--- app/src/test/java/com/example/qr_scape/QR_ScanTest.java
+++ app/src/test/java/com/example/qr_scape/QR_ScanTest.java
@@ -1,13 +1,23 @@
 package com.example.qr_scape;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import android.content.SharedPreferences;
+
 import java.nio.charset.StandardCharsets;
 
 public class QR_ScanTest {
 
-    @Test
-    public void addQRCodeTest(){
-
-    }
+//    @Test
+//    public void addQRCodeTest(){
+//        final String USERNAME = "Username";
+//        // Check shared preferences for username
+//        SharedPreferences sharedPreferences;
+//        sharedPreferences = getSharedPreferences(String.valueOf(R.string.app_name),MODE_PRIVATE);
+//        String username = sharedPreferences.getString(USERNAME,null);
+//        assertEquals(1,2);
+//    }
 }
