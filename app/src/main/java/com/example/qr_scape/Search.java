a7b0fcd2e0c4a36e50c63baf78eff452edc5bba7
Dallin Dmytryk <dallindmytryk@gmail.com>
2022-03-16 19:53:21 -0600

    added layout for search, skeleton code for funtionality

--- app/src/main/java/com/example/qr_scape/Search.java
+++ app/src/main/java/com/example/qr_scape/Search.java
@@ -1,23 +1,65 @@
+/*
+ * Search
+ *
+ * March 16 2022
+ *
+ * Version 1
+ *
+ * Copyright 2022 Dallin Dmytryk, Harsh Shah, Kiran Deol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.qr_scape;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+/**
+ * Search
+ * activity for searching users or qrcodes
+ * allows text entry
+ * then lists results
+ * @author Dallin Dmytryk
+ * @version 1
+ */
 public class Search extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    EditText searchField;
+    Button modeSelector;
+    Button searchButton;
+    ListView playerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.background));
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.toolbar_title_layout);
+
+        // Set bottom navigation
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.nav_search);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -46,20 +88,18 @@ public class Search extends AppCompatActivity {
                 return false;
             }
         });
-//        scantext=(TextView)findViewById(R.id.scantext);
-//        scanbtn=(Button) findViewById(R.id.scanbtn);
-//
-//        scanbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(getApplicationContext(),QR_Scan.class));
-//            }
-//        });
 
+        // Set search bar
+        searchField = findViewById(R.id.search_terms_edittext);
 
+        // Set mode button
+        modeSelector = findViewById(R.id.search_setting_button);
 
+        // Set search button
+        searchButton = findViewById(R.id.search_search_button);
 
-
+        // Set player list
+        playerList = findViewById(R.id.search_player_result_listview);
 
     }
 }
