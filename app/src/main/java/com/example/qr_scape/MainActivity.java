1cb1157db1da53941b1f65df2f8621952103850b
KashSansanwal <kashish.sansanwal39@gmail.com>
2022-03-17 18:23:36 -0600

    New QRCollections
    Better version of the Collections page. (Still in progress)

--- app/src/main/java/com/example/qr_scape/MainActivity.java
+++ app/src/main/java/com/example/qr_scape/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this,LoginActivity.class);
+        Intent intent = new Intent(this,QRCollectionActivity.class);
         startActivity(intent);
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
